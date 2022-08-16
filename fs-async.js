84b214649bd636939f5cfb7a551a7850b49e7962
Nourhen Arfaoui <nourarfeoui20@gmail.com>
2022-08-16 12:01:44 +0100

    update result-syncro.txt

--- fs-async.js
+++ fs-async.js
@@ -7,13 +7,13 @@ readFile('./content/first.txt', 'utf8', (err, result_fi) => {
   const first = result_fi
   //err is the first arguemnt and it takes the error and the second takes the result if there is no error
   readFile('./content/second.txt', 'utf8', (err, result_sec) => {
-    if (err) {
+    if (err) { 
       console.log(err)
       return   
     }
     const second = result_sec
     writeFile(
-      './content/result-async.txt',
+      './content/result-asynco.txt',
       `Here is the result : ${first}, ${second}`,
       (err, result_fin) => {
         if (err) {
