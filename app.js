52dc37964939e002f4aa894e4ab6b2c560f17022
Nourhen Arfaoui <nourarfeoui20@gmail.com>
2022-08-16 12:26:20 +0100

    server creation

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const names=require('./3-names');
 console.log(names);
 loc(names.male) ;
 loc(names.female) ;
-*/
 
+const ser=require('./server-creation')
 const { readFileSync, writeFileSync } = require("fs");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
@@ -17,3 +17,17 @@ writeFileSync(
   `here is the result of grap the dirst ans tbe second text :${first},${second}`,
   {flag:'a'}
 );
+*/
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end('this is our home page')
+    }
+    if(req.url === '/about'){
+       res.end('this is about page') 
+    }
+    res.end('error 404 page not found')
+    server.listen(8000,()=>{
+        console.log('server is listening on port 8000')
+    })
+})
